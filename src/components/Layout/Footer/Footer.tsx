111b9d6fdf915a9246aa8567c93f419d1da22291
ricardo-borges13 <ricardo.borges@ymail.com>
2026-01-28 14:22:13 -0300

    ajuste telefone footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -7,13 +7,11 @@ import { APP_VERSION } from '../../../version';
 import type { ContactInfo } from '../../../types/contact';
 import { menuItems } from '../Menu/menuData';
 
-export const Footer = ({ phone, phone2, email }: ContactInfo) => {
+
+export const Footer = ({ phone, phone2, phone3, email }: ContactInfo) => {
   const footerItems = menuItems.filter(item => item.showInFooter);
 
-  const phoneHref = `tel:${phone.replace(/[^\d+]/g, '')}`;
-  const phoneHref2 = phone2
-    ? `tel:${phone2.replace(/[^\d+]/g, '')}`
-    : null;
+  const getPhoneHref = (phone: string) => `tel:${phone.replace(/[^\d+]/g, '')}`;
 
   const emailHref = `mailto:${email}`;
 
@@ -39,7 +37,7 @@ export const Footer = ({ phone, phone2, email }: ContactInfo) => {
                 </li>
               ))}
               <li>
-                <Link to="/contato">Entre em Contato</Link>
+                <Link to="/orcamento">Orçamento</Link>
               </li>
             </ul>
           </S.Block>
@@ -49,13 +47,21 @@ export const Footer = ({ phone, phone2, email }: ContactInfo) => {
             <address>
               <span>
                 <BiSolidPhoneOutgoing />
-                <a href={phoneHref}>{phone}</a>
+                <a href={getPhoneHref(phone)}> {phone}</a>
               </span>
 
-              {phoneHref2 && (
+              {phone2 && (
                 <span>
                   <BiSolidPhoneOutgoing />
-                  <a href={phoneHref2}>{phone2}</a>
+
+                  <a href={getPhoneHref(phone2)}>{phone2}</a>
+                </span>
+              )}
+              {phone3 && (
+                <span>
+                  <BiSolidPhoneOutgoing />
+
+                  <a href={getPhoneHref(phone3)}>{phone3}</a>
                 </span>
               )}
 
@@ -72,9 +78,10 @@ export const Footer = ({ phone, phone2, email }: ContactInfo) => {
               <span>Consulter Soluções</span>
               <span>CNPJ: 01.111.222/0001-01</span>
               <span>
-                Rua das Canoas, 1180, Loja 2 – Bairro Estrela Dalva do Oriente
+                Rua das Canoas, 1180, Loja 2 Bairro Estrela Dalva do Oriente
               </span>
-              <span>Belo Horizonte – MG • CEP: 30.421-194</span>
+              <span>Belo Horizonte - MG </span>
+              <span>CEP: 30.421-194</span>
             </address>
           </S.Block>
         </S.LinksArea>
